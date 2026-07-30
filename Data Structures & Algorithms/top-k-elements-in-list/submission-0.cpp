c0c2fcd7c4a3e7b71e8d113c0afffb5ad9b81975
AKASHK0007 <140057581+AKASHK0007@users.noreply.github.com>
2026-07-30 20:40:30 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    static bool comparator(const pair<int, int>& a, const pair<int, int>& b) {
+        return a.second > b.second;
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        vector<pair<int,int>>vec(mp.begin(),mp.end());
+        sort(vec.begin(), vec.end(), comparator);
+        vector<int> ans;
+        for(int i=0; i<k; i++){
+            ans.push_back(vec[i].first);
+        }
+        return ans;
+    }
+};
