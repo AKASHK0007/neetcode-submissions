c937dcb145647f669edc70b10d145d40ae65f136
AKASHK0007 <140057581+AKASHK0007@users.noreply.github.com>
2026-07-30 19:54:22 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>freq(26,0);
+        for(char ch:s){
+            freq[ch-'a']++;
+        }
+        for(char ch:t){
+            freq[ch-'a']--;
+        }
+        for(int i:freq){
+            if(i){
+                return false;
+            }
+        }
+        return true;
+    }
+};
